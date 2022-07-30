009e2c58366c28774bd0eb82fd1ae6d21e91a599
HeX The Cat <atte.viberg@hotmail.com>
2022-07-30 11:56:36 +0300

    Update PelinEsittely.js

--- src/pages/PelinEsittely.js
+++ src/pages/PelinEsittely.js
@@ -5,14 +5,19 @@ const PelinEsittely = () => {
                 Our Game:
             </h1>
             <p>
-                Project Vesiraukka is the current in-development game by Broken Code Games, thus far only known by project name.<br/><br/>
+                Project Vesiraukka is the current in-development game by Broken Code Games, thus far only known by its project name.<br/><br/>
 
-                The Great War to end all wars has come to a conclusion, but the turmoil and chaos caused by it will still linger in eastern parts of Europe for years to come. Borders are being redrawn by the victorious powers, losing sides are licking their wounds or joining paramilitary groups keen on resisting, while bolshevik forces in Russian Soviet Republic are keen on expanding and rewriting the eastern European borders once more. Affected by all these changes in the middle of them stands young nation of Finland, barely a year old, upon which the bolsheviks have laid their eyes on...<br/><br/>
+                The Great War to end all wars has come to a conclusion on 11.11.1918, but the turmoil and chaos caused by it will still linger in eastern parts of Europe for years to come. Borders are being redrawn by the victorious powers, losing sides are licking their wounds or joining paramilitary groups resisting the occupations, while bolshevik forces in the newly founded Russian Soviet Republic are attempting to push their revolution westward to the old Imperial Russian sphere and beyond. Affected by all these changes in the middle of them stands young nation of Finland, upon which the bolsheviks have laid their eyes on...<br/><br/>
 
-                In Project Vesiraukka, you take command of a steam-operated submarine in the winter of 1918. Operating this out-dated piece of machinery in the frozen Archipelago Sea, separated and isolated from the main forces and chain of command, your mission is to wage a guerilla warfare on sea against the reds, stop them from spreading their influence and and prevent the Russian bolsheviks from shipping in men and material to help them.<br/><br/>
+                In Project Vesiraukka, you take command of a Nordenfelt I-class steam-operated submarine in the winter of 1918. Operating this out-dated piece of machinery in the frozen Archipelago Sea, separated and isolated from the main forces and chain of command, your mission is to wage a naval guerilla warfare on sea against the Red Forces, manage your limited resources and eventually turn the tide against the numerically superior enemy.<br/><br/>
+
+                While we attempt to make the game mechanically, historically and rationally as realistic and accurate as possible, few artistic liberties have been taken:<br/>
+                - Nature; Archipelago Sea is usually completely frozen during winter. In-game winter is made milder, to have the possiblity of actually sailing the waters<br/>
+                - History; Bolsheviks are putting greater emphasis on having influence in southern Finnish coastal cities, thus (hostile) naval traffic is heavier in the game than it historically was<br/>
+                - Mechanical; The submarine, while based on a historical model, is to a degree made with estimates. As there exists only 100+ years old blueprints and very few photographs and little written information, some of the modelling can only be estimated through rough calculations<br/><br/>
 
                 Features:<br/>
-                - Sail and operate steam-powered submarine based on Nordenfelt I-model, managing its steam pressure, fuel economy, field upgrades and more.<br/>
+                - Sail and operate steam-powered submarine based on Nordenfelt I-class, managing its steam pressure, fuel economy, field upgrades and more.<br/>
                 - Recreated Archipelago Sea in 1:1 scale.<br/>
                 - Operate multiple weapons systems; Torpedo, deck gun and naval mines.<br/>
                 - Story progression in changing world; Your actions in previous missions have consequences in your future missions.<br/>
@@ -22,8 +27,8 @@ const PelinEsittely = () => {
                 ✓ Create 3D model of the submarine<br/>
                 ✓ Texture the model of the submarine<br/>
                 ✓ Animate the deck utilities of the submarine<br/>
-                ✗ Program boiler heating functions and limits<br/>
-                ✗ Program steam pressure functions and limits<br/>
+                ✓ Program boiler heating functions and limits<br/>
+                ✓ Program steam pressure functions and limits<br/>
                 ✗ Program power transmission to propellor<br/>
                 ✗ Program forward and backwards mobility<br/>
                 ✗ Program vertical steering<br/>
